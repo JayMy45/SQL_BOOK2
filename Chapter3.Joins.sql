a65d3a018ff7d9e1a608bd174398cdbec31f858a
Jeremy Myers <echelonical@gmail.com>
2023-08-08 22:42:03 -0500

    completed question 4

--- Chapter3.Joins.sql
+++ Chapter3.Joins.sql
@@ -35,5 +35,27 @@ LEFT JOIN customers c ON s.customer_id = c.customer_id
 LEFT JOIN employees e ON s.employee_id = e.employee_id
 LEFT JOIN dealerships d ON s.dealership_id = d.dealership_id;
 
+-- 3. Get a list of all the dealerships and the employees, if any, working at each one.
+SELECT
+d.dealership_id,
+d2.business_name,
+e.first_name emp_firstname,
+e.last_name  emp_lastname
+FROM dealershipemployees d
+LEFT JOIN dealerships d2 ON d2.dealership_id = d.dealership_id
+RIGHT JOIN employees e ON d.dealership_employee_id = e.employee_id; 
+
+-- 4. Get a list of vehicles with the names of the body type, make, model and color.
+SELECT 
+v2.body_type, 
+v2.make ,
+v2.model,
+v.exterior_color
+FROM vehicles v
+LEFT JOIN vehicletypes v2 ON v.vehicle_type_id  = v2.vehicle_type_id;
+
+
+
+
 
 
