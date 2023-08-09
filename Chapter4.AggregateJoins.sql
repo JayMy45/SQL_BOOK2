723dd860a50c4d54b39275a05482252fe7ae03ea
Jeremy Myers <echelonical@gmail.com>
2023-08-09 14:59:03 -0500

    working on aggregate joins in Chapter 4

---
+++ Chapter4.AggregateJoins.sql
@@ -0,0 +1,39 @@
+-- Find employees who haven't made any sales and the name of the dealership they work at.
+SELECT
+    e.first_name,
+    e.last_name,
+    d.business_name,
+    s.price
+FROM employees e
+INNER JOIN dealershipemployees de ON e.employee_id = de.employee_id
+INNER JOIN dealerships d ON d.dealership_id = de.dealership_id
+LEFT JOIN sales s ON s.employee_id = e.employee_id
+WHERE s.price IS NULL;
+
+-- Get all the departments in the database,
+-- all the employees in the database and the floor price of any vehicle they have sold.
+SELECT
+    d.business_name,
+    e.first_name,
+    e.last_name,
+    v.floor_price
+FROM dealerships d
+LEFT JOIN dealershipemployees de ON d.dealership_id = de.dealership_id
+INNER JOIN employees e ON e.employee_id = de.employee_id
+INNER JOIN sales s ON s.employee_id = e.employee_id
+INNER JOIN vehicles v ON s.vehicle_id = v.vehicle_id;
+
+-- Use a self join to list all sales that will be picked up on the same day,
+-- including the full name of customer picking up the vehicle. .
+SELECT
+    CONCAT  (c.first_name, ' ', c.last_name) AS last_name,
+    s1.invoice_number,
+    s1.pickup_date
+FROM sales s1
+INNER JOIN sales s2
+    ON s1.sale_id <> s2.sale_id 
+    AND s1.pickup_date = s2.pickup_date
+INNER JOIN customers c
+   ON c.customer_id = s1.customer_id;
+   
+  
