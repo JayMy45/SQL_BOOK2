292c83261cd9d914a72459ffa1e79c6654bec6d0
Jeremy Myers <echelonical@gmail.com>
2023-08-08 19:40:44 -0500

    query for listing sales records where sales was a lease

--- Chapter2.sql
+++ Chapter2.sql
@@ -2,10 +2,17 @@
 SELECT *
 FROM salestypes s 
 
--- Get a list of sales records where the sale was a lease.
+-- 1. Get a list of sales records where the sale was a lease.
 SELECT  
-FROM sales s 
-WHERE	s.sales_type_id = 2
+	s.sale_id, s.sales_type_id, s.vehicle_id  
+FROM 
+	sales s 
+WHERE 
+	s.sales_type_id = '2';
+
+-- 2. Get a list of sales where the purchase date is within the last five years.
+
+
 
 SELECT s.sale_id, s.vehicle_id, s.dealership_id, s.sales_type_id, s.deposit, s.payment_method  
 FROM sales s 
